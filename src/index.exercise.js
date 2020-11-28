2a0c30a4232ad4a05adf7a62d14f099a48409468
karl-thomas <karl.thomas925@gmail.com>
2020-11-27 21:14:59 -0600

    fix css declarations on the styled-with-emotion elements

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import {css, jsx} from '@emotion/core'
+import {jsx} from '@emotion/core'
 import 'bootstrap/dist/css/bootstrap-reboot.css'
 import '@reach/dialog/styles.css'
 import * as React from 'react'
@@ -18,10 +18,10 @@ function LoginForm({onSubmit, submitButton}) {
       password: password.value,
     })
   }
+
   return (
     <form
-      onSubmit={handleSubmit}
-      css={css({
+      css={{
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'stretch',
@@ -30,7 +30,8 @@ function LoginForm({onSubmit, submitButton}) {
           width: '100%',
           maxWidth: '300px',
         },
-      })}
+      }}
+      onSubmit={handleSubmit}
     >
       <FormGroup>
         <label htmlFor="username">Username</label>
@@ -56,23 +57,23 @@ function App() {
 
   return (
     <div
-      css={css({
+      css={{
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
         width: '100%',
         height: '100vh',
-      })}
+      }}
     >
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
       <div
-        css={css({
+        css={{
           display: 'grid',
           gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
           gridGap: '0.75rem',
-        })}
+        }}
       >
         <Modal>
           <ModalOpenButton>
