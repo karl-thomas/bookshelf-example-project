5c1653ab7ac2eba1d71c798329ab09c72a87922d
karl-thomas <karl.thomas925@gmail.com>
2020-12-10 02:01:04 -0600

    create an auth provider and provider wrapper for the app

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,15 +1,67 @@
-import {createContext, useContext} from 'react'
-const AuthContext = createContext()
+import * as React from 'react'
+import * as auth from 'auth-provider'
+import {client} from 'utils/api-client'
+import {FullPageErrorFallback, FullPageSpinner} from 'components/lib'
+import {useAsync} from 'utils/hooks'
+
+const AuthContext = React.createContext()
 AuthContext.displayName = 'AuthContext'
 
 function useAuth() {
-  const auth = useContext(AuthContext)
+  const auth = React.useContext(AuthContext)
   if (auth === undefined) {
-    throw new Error(
-      'useAuth must be used within a AuthContext.Provider component',
-    )
+    throw new Error('useAuth must be used within a AuthProvider component')
   }
   return auth
 }
 
-export {AuthContext, useAuth}
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
+
+function AuthProvider({children}) {
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    isSuccess,
+    run,
+    setData,
+  } = useAsync()
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
+
+  const login = form => auth.login(form).then(user => setData(user))
+  const register = form => auth.register(form).then(user => setData(user))
+  const logout = () => {
+    auth.logout()
+    setData(null)
+  }
+
+  const value = {user, login, register, logout}
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error} />
+  }
+
+  if (isSuccess) {
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+  }
+}
+
+export {AuthProvider, useAuth}
--- src/context/index.exercise.js
+++ src/context/index.exercise.js
@@ -1,2 +1,28 @@
 // this module doesn't do anything for the exercise. But you'll use this for
 // the extra credit!
+import * as React from 'react'
+import {AuthProvider} from 'context/auth-context'
+import {ReactQueryConfigProvider} from 'react-query'
+import {Router} from 'react-router'
+
+const queryConfig = {
+  retry(failureCount, error) {
+    if (error.status === 404) return false
+    else if (failureCount < 2) return true
+    else return false
+  },
+  useErrorBoundary: true,
+  refetchAllOnWindowFocus: false,
+}
+
+function AppProviders({children}) {
+  return (
+    <ReactQueryConfigProvider config={queryConfig}>
+      <Router>
+        <AuthProvider>{children}</AuthProvider>
+      </Router>
+    </ReactQueryConfigProvider>
+  )
+}
+
+export {AppProviders}
