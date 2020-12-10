875130ebf05c619da92d3fac63f21f90a9d62e32
karl-thomas <karl.thomas925@gmail.com>
2020-12-10 02:22:00 -0600

    create a hook to mange an authenticated client request

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -64,4 +64,14 @@ function AuthProvider({children}) {
   }
 }
 
-export {AuthProvider, useAuth}
+function useClient() {
+  const {
+    user: {token},
+  } = useAuth()
+  return React.useCallback(
+    (endpoint, config = {}) => client(endpoint, {...config, token}),
+    [token],
+  )
+}
+
+export {AuthProvider, useAuth, useClient}
