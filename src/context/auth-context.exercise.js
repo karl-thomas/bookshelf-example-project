41ef26f8066e33a4179bf4641ff4478af3af9aa6
karl-thomas <karl.thomas925@gmail.com>
2020-12-10 01:29:30 -0600

    create an auth context to distribute our authentication logic

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,2 +1,15 @@
-// 🐨 create and export a React context variable for the AuthContext
-// 💰 using React.createContext
+import {createContext, useContext} from 'react'
+const AuthContext = createContext()
+AuthContext.displayName = 'AuthContext'
+
+function useAuth() {
+  const auth = useContext(AuthContext)
+  if (auth === undefined) {
+    throw new Error(
+      'useAuth must be used within a AuthContext.Provider component',
+    )
+  }
+  return auth
+}
+
+export {AuthContext, useAuth}
