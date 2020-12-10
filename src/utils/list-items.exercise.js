077e484eed6b19cec00a0581033814190fd0fa51
karl-thomas <karl.thomas925@gmail.com>
2020-12-10 00:32:53 -0600

    optimistically update list items

--- src/utils/list-items.exercise.js
+++ src/utils/list-items.exercise.js
@@ -26,11 +26,28 @@ function useListItems(user) {
 }
 
 const defaultMutationOptions = {
+  onError: (err, variables, recover) =>
+    typeof recover === 'function' ? recover() : null,
   onSettled: () => queryCache.invalidateQueries('list-items'),
 }
 
 function useUpdateListItem({token}, options = {}) {
-  const mutationOptions = {...defaultMutationOptions, ...options}
+  const mutationOptions = {
+    onMutate(newItem) {
+      const previousItems = queryCache.getQueryData('list-items')
+
+      queryCache.setQueryData('list-items', old =>
+        old.map(item =>
+          item.id === newItem.id ? {...item, ...newItem} : item,
+        ),
+      )
+
+      return () => queryCache.setQueryData('list-items', previousItems)
+    },
+    ...defaultMutationOptions,
+    ...options,
+  }
+
   return useMutation(
     data => client(`list-items/${data.id}`, {method: 'PUT', data, token}),
     mutationOptions,
@@ -38,7 +55,20 @@ function useUpdateListItem({token}, options = {}) {
 }
 
 function useRemoveListItem({token}, options = {}) {
-  const mutationOptions = {...defaultMutationOptions, ...options}
+  const mutationOptions = {
+    onMutate(removedItem) {
+      const previousItems = queryCache.getQueryData('list-items')
+
+      queryCache.setQueryData('list-items', old => {
+        return old.filter(item => item.id !== removedItem.id)
+      })
+
+      return () => queryCache.setQueryData('list-items', previousItems)
+    },
+    ...defaultMutationOptions,
+    ...options,
+  }
+
   return useMutation(
     ({id}) => client(`list-items/${id}`, {method: 'DELETE', token}),
     mutationOptions,
