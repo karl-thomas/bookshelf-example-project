a3c35592f6bbb82c171bf032b30a9d91a75edcea
karl-thomas <karl.thomas925@gmail.com>
2020-12-11 03:09:42 -0600

    add profiling to some of our components

--- src/components/profiler.exercise.js
+++ src/components/profiler.exercise.js
@@ -1,1 +1,30 @@
-// this is for extra credit
+import * as React from 'react'
+import {client} from 'utils/api-client'
+
+function Profiler({id, metadata, children}) {
+  function handleRender(
+    id, // the "id" prop of the Profiler tree that has just committed
+    phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+    actualDuration, // time spent rendering the committed update
+    baseDuration, // estimated time to render the entire subtree without memoization
+    startTime, // when React began rendering this update
+    commitTime, // when React committed this update
+    interactions, // the Set of interactions belonging to this update
+  ) {
+    client('profile', {
+      data: {
+        id,
+        metadata,
+        phase,
+        actualDuration,
+        baseDuration,
+        startTime,
+        commitTime,
+        interactions,
+      },
+    })
+  }
+  return <React.Profiler id={id} onRender={handleRender} children={children} />
+}
+
+export {Profiler}
--- src/index.exercise.js
+++ src/index.exercise.js
@@ -4,12 +4,15 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 import {App} from './app'
 import {AppProviders} from './context'
+import {Profiler} from 'components/profiler'
 
 loadDevTools(() => {
   ReactDOM.render(
-    <AppProviders>
-      <App />
-    </AppProviders>,
+    <Profiler id="Root of App">
+      <AppProviders>
+        <App />
+      </AppProviders>
+    </Profiler>,
     document.getElementById('root'),
   )
 })
