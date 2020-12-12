40cc5b6f4ad9bd0ea6f56596b9a3373a0c928c0c
karl-thomas <karl.thomas925@gmail.com>
2020-12-12 05:04:53 -0600

    fill out the use cases more

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -5,10 +5,13 @@ import {
   screen,
   waitForLoadingToFinish,
   userEvent,
+  createAuthUser,
 } from 'test/app-test-utils'
-import {buildBook} from 'test/generate'
+import {buildBook, buildListItem} from 'test/generate'
 import {App} from 'app'
 import * as booksDB from 'test/data/books'
+import * as listItemsDB from 'test/data/list-items'
+import {formatDate} from 'utils/misc'
 
 test('renders all the book information', async () => {
   const book = await booksDB.create(buildBook())
@@ -43,4 +46,70 @@ test('can create a list item for the book', async () => {
     screen.getByRole('button', {name: /mark as read/i}),
   ).toBeInTheDocument()
   expect(screen.getByLabelText(/notes/gi)).toBeInTheDocument()
+})
+
+test('can remove a list item for the book', async () => {
+  const book = await booksDB.create(buildBook())
+  const route = `/book/${book.id}`
+  const user = await createAuthUser()
+  await listItemsDB.create(buildListItem({owner: user, book}))
+
+  await render(<App />, {route, user})
+
+  userEvent.click(
+    screen.getByRole('button', {
+      name: /remove from list/i,
+    }),
+  )
+
+  expect(
+    screen.queryByRole('button', {name: /add to list/i}),
+  ).toBeInTheDocument()
+})
+
+test('can mark a list item as read', async () => {
+  jest.useFakeTimers()
+  const book = await booksDB.create(buildBook())
+  const route = `/book/${book.id}`
+  const user = await createAuthUser()
+  const listItem = await listItemsDB.create(buildListItem({owner: user, book}))
+
+  await render(<App />, {route, user})
+
+  userEvent.click(
+    screen.getByRole('button', {
+      name: /mark as read/i,
+    }),
+  )
+  await waitForLoadingToFinish()
+
+  expect(
+    screen.findByRole('button', {name: /mark as unread/i}),
+  ).toBeInTheDocument()
+  expect(screen.getByLabelText(/start and finish date/i)).toHaveTextContent(
+    `${formatDate(listItem.startDate)} — ${formatDate(Date.now())}`,
+  )
+})
+
+test('can edit a note', async () => {
+  jest.useFakeTimers()
+  const book = await booksDB.create(buildBook())
+  const route = `/book/${book.id}`
+  const user = await createAuthUser()
+  const listItem = await listItemsDB.create(buildListItem({owner: user, book}))
+
+  const notes = 'VYre SPECIaL NOTeS'
+  const notesTextarea = screen.getByRole('textbox', {name: /notes/i})
+
+  await render(<App />, {route, user})
+  userEvent.clear(notesTextarea)
+  userEvent.type(notesTextarea, notes)
+  await screen.findByLabelText(/loading/i)
+  await waitForLoadingToFinish()
+
+  expect(notesTextarea).toHaveValue(notes)
+
+  expect(await listItemsDB.read(listItem.id)).toMatchObject({
+    notes,
+  })
 })
--- src/setupTests.exercise.js
+++ src/setupTests.exercise.js
@@ -1,10 +1,13 @@
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom/extend-expect'
 import {queryCache} from 'react-query'
+import {act} from '@testing-library/react'
 import {server} from 'test/server'
 import * as auth from 'auth-provider'
 import * as usersDB from 'test/data/users'
 import * as booksDB from 'test/data/books'
 import * as listItemsDB from 'test/data/list-items'
+
+jest.mock('components/profiler')
 
 // enable API mocking in test runs using the same request handlers
 // as for the client-side mocking.
@@ -21,4 +24,11 @@ afterEach(async () => {
     booksDB.reset(),
     listItemsDB.reset(),
   ])
+})
+
+afterEach(() => {
+  if (jest.isMockFunction(setTimeout)) {
+    act(() => jest.runOnlyPendingTimers())
+    jest.useRealTimers()
+  }
 })
